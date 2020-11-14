584bdc7982830bd936d6cdcaa92bb36ab3b71c7d
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-15 06:11:27 +0900

    sort date

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { isPosts } from "../../../entity/Post";
 import { store } from "../../../infra/FirebaseServer";
 
-export default async (request: NextApiRequest, response: NextApiResponse) => {
+export default async (_: NextApiRequest, response: NextApiResponse) => {
   const documents = await store.collection("posts").get();
   const posts = documents.docs.map((d) => {
     const post = {
@@ -10,5 +11,11 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
     };
     return post;
   });
-  response.json(posts);
+  if (!isPosts(posts)) {
+    throw new Error("invalid data");
+  }
+  const sortedPosts = posts.sort((a, b) =>
+    a.createdDate < b.createdDate ? 1 : -1
+  );
+  response.json(sortedPosts);
 };
