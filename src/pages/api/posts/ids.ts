02aa3cc7e0be70a05df5aeffa5e4a384ade9ce7b
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-14 23:04:13 +0900

    fix all private key

--- src/pages/api/posts/ids.ts
+++ src/pages/api/posts/ids.ts
@@ -4,7 +4,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 const cert = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
 };
 try {
   admin.initializeApp({
