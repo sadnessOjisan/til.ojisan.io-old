a51d33ebe81358dea99ad2d5bef51ce7d0b1771a
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-14 19:45:54 +0900

    connect firestore

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
+const _genEnv = () => {
+  const mode = process.env.NODE_ENV;
+  if (mode === "development") {
+    console.log("ddddeeee");
+    return {
+      NEXT_PUBLIC_ENDPOINT: "http://localhost:3000",
+    };
+  }
+};
+
 module.exports = {
-  env: {
-    NEXT_PUBLIC_ENDPOINT: "http://localhost:3000",
-  },
+  env: _genEnv(),
 };
--- src/pages/api/posts/[pid].ts
+++ src/pages/api/posts/[pid].ts
@@ -1,17 +1,24 @@
-// import * as admin from "firebase-admin";
+import * as admin from "firebase-admin";
 import { NextApiRequest, NextApiResponse } from "next";
+import { shape } from "prop-types";
 
-// const cert = {
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-//   // zeit now の環境変数だと\nが\\nにエスケープされてしまっているので元に戻す? .replace(/\\n/g, "\n")
-//   privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-// };
-// admin.initializeApp({
-//   credential: admin.credential.cert(cert),
-// });
+const cert = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  // zeit now の環境変数だと\nが\\nにエスケープされてしまっているので元に戻す? .replace(/\\n/g, "\n")
+  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+};
+console.log("certcertcertcert", cert);
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert(cert),
+  });
+} catch (e) {
+  console.log(e);
+}
+console.log("certcertcertcert", cert);
 
-// const store = admin.firestore();
+const store = admin.firestore();
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
   const {
@@ -21,7 +28,10 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
     console.error("pid", pid);
     throw new Error("invalid request");
   }
-  // const data = await (await store.doc("").get()).data();
-  const data = { id: pid, title: "sample" };
-  response.json(data);
+  const snapshot = await store
+    .collection("posts")
+    .doc("GnDSL3Bg3rCtmgLJpkTp")
+    .get();
+  const data = await snapshot.data();
+  response.json({ id: snapshot.id, ...data });
 };
