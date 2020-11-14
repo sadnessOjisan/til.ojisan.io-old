f54b73c2f777a52483fe2f2d9e676b66dcbe00cd
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-14 23:08:36 +0900

    fix

--- src/repository/getPostIds.ts
+++ src/repository/getPostIds.ts
@@ -6,13 +6,13 @@ export const getPostIds = async (): Promise<ApiResponseType<PostIdsType>> => {
   const response = await Fetch(`api/posts/ids`);
   if (response.status !== 200) {
     console.error("<getPostIds>invalid status");
-    return { data: undefined, error: "invalid status error" };
+    return { data: undefined, error: (response.status as any) as string };
   }
   const data = await response.json();
   if (isIds(data)) {
     return { data, error: undefined };
   } else {
     console.error("<getPostIds> invalid data struct");
-    return { data: undefined, error: "invalid data struct" };
+    return { data: undefined, error: data };
   }
 };
