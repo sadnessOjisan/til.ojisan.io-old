4ec862bed2a93ab62d992a7f667bb0332105ec9c
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-17 07:04:54 +0900

    summary

--- src/pages/dates/[date].tsx
+++ src/pages/dates/[date].tsx
@@ -1,16 +1,52 @@
 import { GetStaticProps } from "next";
+import styled from "styled-components";
+import { Layout } from "../../components/Layout";
+import { PostListItem } from "../../components/PostListItem";
+import { Color } from "../../const/color";
 import {
   createPostForView,
   createURLFormattedDate,
   isValidDate,
+  PostViewType,
 } from "../../entity/Post";
 import { getPostAllDates } from "../../repository/getPostAllDates";
 import { getPostsByDate } from "../../repository/getPostsByDate";
 import { getTags } from "../../repository/getTags";
 
-const Component = (props) => {
-  return <div>hoge{JSON.stringify(props)}</div>;
+type Props = {
+  posts?: PostViewType[];
+  error?: string;
+  className?: string;
+  date?: string;
 };
+
+const Component = (props: Props) => (
+  <Layout>
+    {JSON.stringify(props)}
+    <div className={props.className}>
+      <h1>
+        {props.date}の<span>T</span>oday oj<span>I</span>san <span>L</span>
+        earned
+      </h1>
+      {props.posts
+        ? props.posts.map((post) => (
+            <PostListItem post={post} key={post.id}></PostListItem>
+          ))
+        : JSON.stringify(props.error)}
+    </div>
+  </Layout>
+);
+
+const StyledComponent = styled(Component)`
+  color: white;
+  & > h1 {
+    font-size: 40px;
+    text-align: center;
+    & > span {
+      color: ${Color.highlight};
+    }
+  }
+`;
 
 export const getStaticProps: GetStaticProps = async (context) => {
   // YYYY-MM-DD
@@ -35,8 +71,8 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
   return {
     // HACK: undefined は埋め込めないため
-    props: !error ? { posts: sortedData } : { error },
-    revalidate: 3000,
+    props: !error ? { posts: sortedData, date: date } : { error },
+    revalidate: 3,
   };
 };
 
@@ -53,4 +89,4 @@ export async function getStaticPaths() {
   };
 }
 
-export default Component;
+export default StyledComponent;
